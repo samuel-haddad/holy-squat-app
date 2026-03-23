e4194c96f7ec7a6151ee1696d7e6f98dd56487db
Samuel Haddad <smachadd@gmail.com>
2026-03-22 22:31:35 -0300

    Atualizando ícone PWA e nome do App

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2604653686" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3553951851" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
